2ec684d519b5a1ee3a426ab2abfb2545ca8c1314
Agnieszka Pachuta <agnieszka@localhost.localdomain>
2014-02-28 01:11:18 +0100

    Add extended menu. Change sending function to serv_send_im

--- src/hka-plugin.c
+++ src/hka-plugin.c
@@ -19,7 +19,14 @@
 #include "server.h"
 #include "status.h"
 
-struct _hka_protocol {
+const gchar SPECIAL_TAG = 126;
+
+const gchar INIT = 33;
+const gchar INIT_RESPONSE = 34;
+const gchar SEND_CAPTCHA = 35;
+const gchar SEND_CAPTCHA_RESPONSE = 36;
+
+struct HumanKeyAgreementProtocol {
 
         //captcha
         void (*create_captcha)(gchar** imgData, gsize* imgSize);
@@ -33,25 +40,81 @@ struct _hka_protocol {
 
 typedef struct __attribute__((__packed__)) {
         gchar tag;
-        gint id;
-} _header;
+        gchar id;
+        gchar stringMsg[0]
+} Message;
 
 typedef struct __attribute__((__packed__)) {
-        _header header;
         gsize size;
         gchar data[0];
-} _message;
-
+} DataMessage;
 
 
 static void
-hka_send(PurpleConversation *conv, int id, const gchar* data, gsize dataSize) 
+hka_send_text(PurpleBuddy* buddy, gchar id, const gchar* text) 
+{
+
+        Message* msg;
+        gsize msgSize;
+        PurpleConnection* connection;
+        PurpleAccount* account = purple_buddy_get_account(buddy);
+        const gchar* name = purple_buddy_get_name(buddy);
+
+        // get connection
+        connection = purple_account_get_connection(account);
+        if(!connection) {
+                purple_debug_misc("hka-plugin", "hka_send_text (no connection)\n"); 
+                //TODO
+        }
+
+        // prepare message
+        msgSize = sizeof(Message) + strlen(text) + 1;
+        msg = (Message*) g_malloc(msgSize);
+        msg->tag = SPECIAL_TAG;
+        msg->id = id;
+        strcpy(msg->stringMsg, text);
+
+        // send message
+        serv_send_im(connection, name, (gchar*)msg, 0);
+
+        g_free(msg);
+
+}
+
+static void
+hka_send(PurpleBuddy* buddy, gchar id, const gchar* data, gsize dataSize)
+{
+
+        gchar* encodedDataMsg;
+        DataMessage* dataMsg;
+        gsize dataMsgSize;
+
+        // prepare and encode data
+        dataMsgSize = sizeof(DataMessage) + dataSize;
+        dataMsg = (DataMessage*) g_malloc(dataMsgSize);
+        dataMsg->size = dataSize;
+        memcpy(dataMsg->data, data, dataSize);
+        encodedDataMsg = HKA.binary_to_text_encode((gchar*)dataMsg, dataMsgSize);
+
+        // send text message
+        hka_send_text(buddy, id, encodedDataMsg); 
+        
+        g_free(dataMsg);
+        g_free(encodedDataMsg);
+
+
+}
+
+
+/*
+static void
+hka_send(PurpleConversation *conv, gchar id, const gchar* data, gsize dataSize) 
 {
         gchar* encodedMsg;
-        gsize msgSize = sizeof(_message) + dataSize;
-        _message* msg = (_message*) g_malloc(msgSize);
+        gsize msgSize = sizeof(Message) + dataSize;
+        Message* msg = (Message*) g_malloc(msgSize);
         
-        msg->header.tag = 0x01;
+        msg->header.tag = SPECIAL_TAG;
         msg->header.id = id;
         msg->size = dataSize;
         
@@ -59,11 +122,35 @@ hka_send(PurpleConversation *conv, int id, const gchar* data, gsize dataSize)
 
         encodedMsg = HKA.binary_to_text_encode((gchar*)msg, msgSize);
 
-        purple_conv_im_send(PURPLE_CONV_IM(conv), encodedMsg);
+        purple_conv_im_send(PURPLE_CONV_IM(conv), (gchar*)msg); //encodedMsg);
 
         g_free(msg);
         g_free(encodedMsg);
 }
+
+
+static void
+hka_send_text(PurpleConversation *conv, gchar id, const gchar* text) 
+{
+        gsize textSize = strlen(text);
+        gsize msgSize = sizeof(TextMessage) + textSize;
+        TextMessage* textMsg = (TextMessage*) g_malloc(msgSize);
+        gchar* encodedTag = HKA.binary_to_text_encode(&SPECIAL_TAG, 1);
+        gchar* encodedId = HKA.binary_to_text_encode(&id, 1);
+
+        textMsg->header.tag = *encodedTag;
+        textMsg->header.id = *encodedId;
+        
+        strcpy(textMsg->text, text);
+        
+        purple_conv_im_send(PURPLE_CONV_IM(conv), (gchar*) textMsg); 
+  
+        g_free(textMsg);
+        g_free(encodedTag);
+        g_free(encodedId);
+}
+
+*/
 
 static void
 load_image(gchar** imgData, gsize* imgSize) {
@@ -90,8 +177,8 @@ sending_im_msg_cb(PurpleAccount *account, char *recipient, char **buffer, void *
 
 static void
 conversation_created_cb(PurpleConversation *conv, gpointer handle) //void *data)
-{
-       hka_send(conv, 1, "lalala", 6);
+{ 
+      
 }
 
 
@@ -99,12 +186,72 @@ static gboolean
 receiving_im_msg_cb(PurpleAccount *account, char **sender, char **buffer,
                                      PurpleConversation *conv, PurpleMessageFlags *flags, void *data)
 {
-      
+        
+/*
+        Message* msg = (Message*) *buffer;
+        gchar* state = (gchar*) purple_conversation_get_data(conv, "hka-protocol-state");
+
+        if(msg->tag == SPECIAL_TAG && msg->id == *state) {
+                //if()
+
+
+
+
+                return TRUE;
+        }
+*/
+         
         return FALSE;
 }
 
 
+static void
+hka_start_protocol_cb(PurpleBlistNode *node, gpointer data)
+{
+        PurpleBuddy* buddy;
 
+        if(!PURPLE_BLIST_NODE_IS_BUDDY(node))
+                return;
+
+        buddy = (PurpleBuddy*) node;
+
+        hka_send_text(buddy, INIT, " Nie masz mojego super pluginu :(");
+
+/*
+        int buddyInt;
+        buddyInt = purple_blist_node_get_int(node, "buddy-int");
+        buddyInt++;
+        purple_blist_node_set_int(node, "buddy-int", buddyInt);
+
+        purple_debug_misc("kiss test", "start_protocol_cb (buddy-int = %d)\n", buddyInt); 
+
+*/
+}
+ 
+
+
+// extended menu
+static void
+blist_node_extended_menu_cb(PurpleBlistNode *node, GList **m)
+{
+        PurpleMenuAction* bna = NULL;
+        
+        // add extended menu only for PurpleBuddy      
+        if(!PURPLE_BLIST_NODE_IS_BUDDY(node))
+                return;
+ 
+        if (purple_blist_node_get_flags(node) & PURPLE_BLIST_NODE_FLAG_NO_SAVE)
+                return;
+
+        *m = g_list_append(*m, bna);
+        bna = purple_menu_action_new(_("START PROTOCOL"), PURPLE_CALLBACK(hka_start_protocol_cb), NULL, NULL);
+        *m = g_list_append(*m, bna);
+        
+}
+
+
+
+// LOAD
 static gboolean
 plugin_load(PurplePlugin *plugin)
 {
@@ -122,7 +269,12 @@ plugin_load(PurplePlugin *plugin)
         purple_signal_connect(conversation, "receiving-im-msg",
                                                plugin, PURPLE_CALLBACK(receiving_im_msg_cb), NULL);
 
-        purple_debug_misc("hka", "plugin-load - EMPTY PLUGIN\n");
+        purple_signal_connect(purple_blist_get_handle(), "blist-node-extended-menu",
+                                               plugin, PURPLE_CALLBACK(blist_node_extended_menu_cb), NULL);
+
+
+
+        purple_debug_misc("hka-plugin", "plugin-load\n");
 
 
         HKA.binary_to_text_encode = g_base64_encode;
